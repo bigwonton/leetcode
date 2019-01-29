8b50c3a8f7797282096c4ab1e2db24c6951b8f27
XinyingShi <xinyingshi@126.com>
2019-01-29 17:32:18 +0800

    0003

--- src/L0003LongestSubstringWithoutRepeatingCharacters/Solution.java
+++ src/L0003LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -1,4 +1,31 @@
 package L0003LongestSubstringWithoutRepeatingCharacters;
 
-public class Solution {
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+        Set<Character> chs = new HashSet<>();
+        int left = 0, right = 0;
+        int res = 0;
+        while (left < length && right < length) {
+            Character ch = s.charAt(right);
+            if (!chs.contains(ch)) {
+                chs.add(ch);
+                right++;
+                res = Math.max(res, right - left);
+            } else {
+                chs.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        s = "abcabcbb";
+        new Solution().lengthOfLongestSubstring(s);
+    }
 }
